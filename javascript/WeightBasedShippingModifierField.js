dec4f2f6687fe180b1841bb8b7cca26ac29e81e4
Will <will@platocreative.co.nz>
2015-02-03 18:40:53 +1300

    updating reference

--- javascript/WeightBasedShippingModifierField.js
+++ javascript/WeightBasedShippingModifierField.js
@@ -1,26 +1,27 @@
 jQuery.noConflict();
 jQuery(document).ready(function($) {
-	
-	$.entwine('#OrderForm_OrderForm', function($){
-		function showNoShippingError(){
-			$('section.payment-details, div.Actions').hide();
-			$('.order-details').append('<div id="shippingNotice" class="message bad"><h3>Shipping Error</h3><p>There are currently no shipping methods available for the region you have selected.</p></div>');	
-		}
-		
-		if($('#OrderForm_OrderForm_Modifiers-WeightBasedShippingModification').length < 1){
-			showNoShippingError();
-		}
-		
-		$('#OrderForm_OrderForm_Modifiers-WeightBasedShippingModification').entwine({
-			onmatch : function() {
-				if($('#shippingNotice').length > 0){
-					$('#shippingNotice').hide().remove();
-					$('section.payment-details, div.Actions').show();
-				}
-			},
-			onunmatch : function() {
+	if($('#shipping').length > 0){
+		$.entwine('sws', function($){
+			function showNoShippingError(){
+				$('section.payment-details, div.Actions').hide();
+				$('.order-details').append('<div id="shippingNotice" class="message bad"><h3>Shipping Error</h3><p>There are currently no shipping methods available for the region you have selected.</p></div>');	
+			}
+			
+			if($('#OrderForm_OrderForm_Modifiers-WeightBasedShippingModification').length < 1){
 				showNoShippingError();
 			}
+			
+			$('#OrderForm_OrderForm_Modifiers-WeightBasedShippingModification').entwine({
+				onmatch : function() {
+					if($('#shippingNotice').length > 0){
+						$('#shippingNotice').hide().remove();
+						$('section.payment-details, div.Actions').show();
+					}
+				},
+				onunmatch : function() {
+					showNoShippingError();
+				}
+			});
 		});
-	});
+	}
 });
