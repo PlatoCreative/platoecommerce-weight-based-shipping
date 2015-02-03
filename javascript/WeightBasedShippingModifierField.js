2ea8e089189db1a4f5dd893d4394bd196ed572c6
Will <will@platocreative.co.nz>
2015-02-03 18:38:10 +1300

    upadting to only fire js on actual checkout page

--- javascript/WeightBasedShippingModifierField.js
+++ javascript/WeightBasedShippingModifierField.js
@@ -1,6 +1,7 @@
 jQuery.noConflict();
 jQuery(document).ready(function($) {
-	$.entwine('sws', function($){
+	
+	$.entwine('#OrderForm_OrderForm', function($){
 		function showNoShippingError(){
 			$('section.payment-details, div.Actions').hide();
 			$('.order-details').append('<div id="shippingNotice" class="message bad"><h3>Shipping Error</h3><p>There are currently no shipping methods available for the region you have selected.</p></div>');	
@@ -17,7 +18,7 @@ jQuery(document).ready(function($) {
 					$('section.payment-details, div.Actions').show();
 				}
 			},
-			onunmatch: function() {
+			onunmatch : function() {
 				showNoShippingError();
 			}
 		});
