79f2343dc6ce249f4d9d46a0ae749bbb32db6421
Myles Derham <mediabeastnz@gmail.com>
2017-08-02 15:11:57 +1200

    Update WeightBasedShippingRate.php

--- code/models/WeightBasedShippingRate.php
+++ code/models/WeightBasedShippingRate.php
@@ -115,13 +115,3 @@ class WeightBasedShippingRate extends DataObject {
 		$this->ShopConfigID = $shopConfig->ID;
 	}
 }
-
-class ProductWeight_Extension extends DataExtension {
-	private static $db = array(
-		'Weight' => 'Decimal(8,4)'
-	);
-
-	public function updateProductCMSFields($fields) {
-		$fields->addFieldToTab('Root.Main', TextField::create('Weight', 'Weight (kgs)'), 'Content');
-	}
-}
